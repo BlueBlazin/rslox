f9c3fb860c593b96193cad068b88482ee4e5f231
Sanjeet N. Dasharath <sanjeetdasharath@gmail.com>
2020-11-20 13:30:21 +0530

    start work on functions

--- src/object.rs
+++ src/object.rs
@@ -1,12 +1,21 @@
+use crate::chunk::Chunk;
 use std::any::Any;
 
 #[derive(Debug)]
 pub enum LoxObj {
     Str(Box<ObjString>),
+    Fun(Box<ObjFunction>),
 }
 
 #[derive(Debug)]
 pub struct ObjString {
     pub length: usize,
     pub value: String,
+}
+
+#[derive(Debug)]
+pub struct ObjFunction {
+    pub arity: usize,
+    pub chunk: Chunk,
+    pub name: ObjString,
 }
